79ff18890100d59e0935c8a97cb73bc20d0d5ba2
anasyakubu <yakubuanas04@gmail.com>
2024-04-22 22:06:58 +0100

    Changes done

---
+++ src/section/Header/Header.tsx
@@ -0,0 +1,14 @@
+import Nav from "../../components/shared/Nav";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="bg-black text-white">
+        <Nav />
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
